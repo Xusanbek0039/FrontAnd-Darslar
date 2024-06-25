782ecf7b0f8adad150e02df5a568c431000b36fa
itparkcebter0071 <109731657+Xusanbek0071@users.noreply.github.com>
2024-06-25 14:37:08 +0500

    Update app.js

--- New-Platform-For-ITC/002/app.js
+++ New-Platform-For-ITC/002/app.js
@@ -3,3 +3,31 @@
 
 
 
+
+
+// let string1 = 'Salom, ';
+// let string2 = 'dunyo!';
+// let birlashtirilganString = string1 + string2;
+// console.log(birlashtirilganString); // Salom, dunyo!
+
+// let birlashtirilganString2 = string1.concat(string2);
+// console.log(birlashtirilganString2); // Salom, dunyo!
+
+
+
+
+
+
+
+// let myString = 'Salom, dunyo!';
+// let qism1 = myString.slice(7, 12); // 'dunyo'
+// let qism2 = myString.substring(7, 12); // 'dunyo'
+// let qism3 = myString.substr(7, 5); // 'dunyo'
+// console.log(qism1, qism2, qism3);
+
+
+
+
+
+
+
