f7d97bd79ece85e5e24bd0bb61c218cfd26e041e
itparkcebter0071 <109731657+Xusanbek0071@users.noreply.github.com>
2024-07-01 17:29:14 +0500

    a

---
+++ Layerlar/app.js
@@ -0,0 +1,11 @@
+let baho = 70;
+
+if (baho >= 90) {
+    console.log("A'lo");
+} else if (baho >= 80) {
+    console.log("Yaxshi");
+} else if (baho >= 70) {
+    console.log("Qoniqarli");
+} else {
+    console.log("Qoniqarsiz");
+}
